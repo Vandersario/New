f023f3ab2e3f37abb1d01b3443856859d42538d9
Vandersario <kig250697@gmail.com>
2021-06-11 19:27:55 +0600

    ytyttt

--- rabota/shop/urls.py
+++ rabota/shop/urls.py
@@ -4,7 +4,5 @@ from . import views
 
 
 urlpatterns=[
-    path('',views.post_list,name='post_list'),
-    path('Cveti/<int:pk>',views.post_detail,name='post_detail'),
-    path('Cveti/new/',views.post_new,name='post_new'),
+    path('',views.mainn,name='mainn'),
 ]
--- rabota/shop/views.py
+++ rabota/shop/views.py
@@ -5,22 +5,9 @@ from .forms import PostForm
 from django.shortcuts import redirect
  
 
-def post_list(request):
-    posts=News.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request,'blog/post_list.html',{'news':posts})
+def mainn(request):
+    return render(request,'blog/main.html',{})
 def post_detail(request,pk):
     post=get_object_or_404(Post,pk=pk)
     return render(request,'blog/post_detail.html',{'news':post})
-def post_new(request):
-    if request.method == "News":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_new.html', {'form': form})
-# Create your views here.
+
